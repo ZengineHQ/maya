104b4e266bbdbeb445f1e17cfe98633ef9daefe5
Roberto Carraretto <rc.carraretto@gmail.com>
2016-04-27 18:25:26 -0300

    frontend build: refactored: extracted file system methods

--- maya/build_canonical.py
+++ maya/build_canonical.py
@@ -1,5 +1,3 @@
-import os
-import fnmatch
 from .wg_config import source_path
 from .wg_config import canonical_build_path
 from .exception import MayaException
@@ -10,6 +8,7 @@ from .util.fs import FileSystem
 class PluginCanonicalCodeBuilder:
 
     def __init__(self, fs, source_path, build_path):
+        self.fs = fs
         self.source_path = source_path
         self.build_path = build_path
         self.scan_path = ScanPath(fs, source_path)
@@ -17,19 +16,14 @@ class PluginCanonicalCodeBuilder:
     def build(self, context):
         self.plugin_name = context['plugin_name']
 
-        plugin_path = self.source_path + '/' + self.plugin_name
-        self.plugin_register_file_path = plugin_path + '/plugin-register.js'
+        self.plugin_path = self.source_path + '/' + self.plugin_name
         self.plugin_build_path = self.build_path + '/' + self.plugin_name
-
-        self.create_plugin_build_path()
         self.scan_paths = self.scan_path.ls(self.plugin_name)
+
+        self.fs.create_dir(self.plugin_build_path)
         self.merge_files_into_one('js')
         self.merge_files_into_one('html')
         self.merge_files_into_one('css')
-
-    def create_plugin_build_path(self):
-        if not os.path.exists(self.plugin_build_path):
-            os.makedirs(self.plugin_build_path)
 
     def merge_files_into_one(self, extension):
         target_file_path = self.create_target_file(extension)
@@ -41,39 +35,22 @@ class PluginCanonicalCodeBuilder:
     def create_target_file(self, extension):
         target_file_name = 'plugin.' + extension
         target_file_path = self.plugin_build_path + '/' + target_file_name
-        open(target_file_path, 'w').close()
+        self.fs.create_file(target_file_path)
         return target_file_path
 
     def merge_all_files_with_extension(self, module_path, extension, target_file_path):
-        path = module_path + '/src'
-
-        files_with_extension = self.get_files_with_extension(path, extension)
-
-        if files_with_extension:
-            self.append_content_of_files(files_with_extension, target_file_path)
-
-    def get_files_with_extension(self, path, extension):
-        files_with_extension = []
-
-        for root, dirnames, filenames in os.walk(path):
-            for filename in fnmatch.filter(filenames, '*.' + extension):
-                files_with_extension.append(os.path.join(root, filename))
-
-        return files_with_extension
-
-    def append_content_of_files(self, source_file_names, target_file_path):
-        with open(target_file_path, 'a') as target_file:
-            for source_file_name in source_file_names:
-                with open(source_file_name) as source_file:
-                    for line in source_file:
-                        target_file.write(line)
-                    target_file.write('\n')
+        src_path = module_path + '/src'
+        file_paths = self.fs.paths_with_extension(src_path, extension)
+        if file_paths:
+            self.fs.append_files(file_paths, target_file_path)
 
     def append_plugin_register(self, target_file_path):
         try:
-            with open(self.plugin_register_file_path) as plugin_register_file:
-                with open(target_file_path, 'a') as target_file:
-                    target_file.write(plugin_register_file.read())
+            plugin_register_path = self.plugin_path + '/plugin-register.js'
+            self.fs.append_file(
+                plugin_register_path,
+                target_file_path
+            )
         except IOError:
             raise MayaException('Build error: plugin register file not found: ' + self.plugin_register_file_path)
 
--- maya/util/fs.py
+++ maya/util/fs.py
@@ -1,12 +1,37 @@
+import os
+import fnmatch
+
+
 class FileSystem:
     def __init__(self):
         self.dirs = {}
 
     def create_dir(self, dir_path):
-        pass
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
 
-    def create_file(self, path, content):
-        pass
+    def create_file(self, path):
+        open(path, 'w').close()
 
     def open(self, path):
         return open(path)
+
+    def paths_with_extension(self, path, extension):
+        paths_with_extension = []
+        for root, dirnames, filenames in os.walk(path):
+            for filename in fnmatch.filter(filenames, '*.' + extension):
+                paths_with_extension.append(os.path.join(root, filename))
+        return paths_with_extension
+
+    def append_files(self, src_paths, dst_file_path):
+        with open(dst_file_path, 'a') as dst_file:
+            for src_path in src_paths:
+                with open(src_path) as src_file:
+                    for line in src_file:
+                        dst_file.write(line)
+                    dst_file.write('\n')
+
+    def append_file(self, src_file_path, dst_file_path):
+        with open(dst_file_path, 'a') as dst_file:
+            with open(src_file_path) as src_file:
+                dst_file.write(src_file.read())
