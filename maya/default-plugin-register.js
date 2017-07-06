b21832f9575721ab236e0cd74c417b6ef5de6cd0
Sean Reid <sareid@outlook.com>
2017-07-06 15:24:54 -0400

    added maya init

---
+++ maya/default-plugin-register.js
@@ -0,0 +1,22 @@
+plugin.register('wgn', {
+    route: '{replace-route}',
+    title: 'Default Registration',
+    icon: 'icon-bookmark-empty',
+    interfaces: [
+    	{
+            controller: 'wgnCntl',
+            template: 'wgn-main',
+            type: 'fullPage',
+            order: 300,
+            topNav: true,
+            routes: [
+                '/:page'
+            ]
+        },
+        {
+            controller: 'wgnSettingsCntl',
+            template: 'wgn-settings',
+            type: 'settings'
+        }
+    ]
+});
